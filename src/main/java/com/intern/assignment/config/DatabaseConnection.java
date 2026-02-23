49d4bb7782448c01bc9af36096154ba550b31d45
bgovil <bgovil@ciena.com>
2026-02-23 23:50:45 +0530

    adding logger to help while debugging

--- src/main/java/com/intern/assignment/config/DatabaseConnection.java
+++ src/main/java/com/intern/assignment/config/DatabaseConnection.java
@@ -3,15 +3,19 @@ package com.intern.assignment.config;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseConnection {
+    private final static Logger logger = LoggerFactory.getLogger(DatabaseConnection.class);
     public static Driver initialise() {
         Driver driver = GraphDatabase.driver(System.getenv("AURADB_URI"),
                 AuthTokens.basic(System.getenv("AURADB_USERNAME"), System.getenv("AURADB_PASSWORD"))
         );
         driver.verifyConnectivity();
+        logger.info("Database initialised successfully");
         return driver;
     }
 }
