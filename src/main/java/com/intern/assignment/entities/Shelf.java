883dc906f933a344cc92b6d3496ee11c35b530eb
bgovil <bgovil@ciena.com>
2026-02-23 17:49:37 +0530

    all the entities created for application

--- src/main/java/com/intern/assignment/entities/Shelf.java
+++ src/main/java/com/intern/assignment/entities/Shelf.java
@@ -1,15 +1,10 @@
 package com.intern.assignment.entities;
 
 import lombok.Data;
-import org.springframework.data.neo4j.core.schema.GeneratedValue;
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
 
-@Node
 @Data
 public class Shelf {
-    @Id @GeneratedValue
-    private String id;
-    private String partNumber;
-    private String shelfName;
+    String id;
+    String name;
+    String partNumber;
 }
--- src/main/java/com/intern/assignment/entities/ShelfPosition.java
+++ src/main/java/com/intern/assignment/entities/ShelfPosition.java
@@ -1,11 +1,9 @@
 package com.intern.assignment.entities;
 
-import org.springframework.data.neo4j.core.schema.GeneratedValue;
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
+import lombok.Data;
 
-@Node
+@Data
 public class ShelfPosition {
-    @Id @GeneratedValue
-    private String id;
+    String id;
+    String deviceId;
 }
