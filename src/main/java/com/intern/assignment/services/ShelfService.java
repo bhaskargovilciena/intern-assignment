fdc2a98bf9b7d73a530ecf3594e259c036be6c32
bgovil <bgovil@ciena.com>
2026-02-24 17:06:46 +0530

    removed unused import statements

--- src/main/java/com/intern/assignment/services/ShelfService.java
+++ src/main/java/com/intern/assignment/services/ShelfService.java
@@ -7,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ShelfService {
     private final ShelfRepository shelfRepository;
